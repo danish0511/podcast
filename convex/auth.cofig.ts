a133d7871230fe87fd542a8c5be24cbd49ca7900
danish0511 <dsdd0511@gmail.com>
2024-06-07 01:12:12 +0530

    new commit

---
+++ convex/auth.cofig.ts
@@ -0,0 +1,10 @@
+const authConfig =  {
+    providers: [
+      {
+        domain: "https://touched-satyr-40.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+export default authConfig;
